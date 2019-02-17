fbf249d7540713dcc6c167cd5dc33bd04380e02d
Florin Anghel <florin.anghel@ugal.ro>
2019-02-17 13:45:09 +0200

    Add work in progress (following tutorial)
    Signed-off-by: Florin Anghel <florin.anghel@ugal.ro>

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+@section('title','About Us')
+
+@section('content')
+    <h1>About Us</h1>
+    <p>
+        <h2>srgzrgzrgefgghds</h2>
+        <h3>ggr dfghyvdev fg ffds</h3>
+        <h3>f4rwer gerwe rwe4 gf</h3>
+    </p>
+
+ @endsection
+
+
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title')</title>
+</head>
+<body>
+
+    <ul>
+       <li><a href="/">Home</a></li>
+      <li><a href="/about">About Us</li>
+      <li><a href="/contact">Contact</li>
+    <ul>
+
+    @yield('content') 
+
+</body>
+</html
